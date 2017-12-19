53e4fa9056954d9d84a2b64415beb13b4b6a1bc5
Moeed Ghori <moeed@texpo.local>
2017-12-19 15:44:58 +0500

    Hell YEah

--- src/components/movies/moviesPage.js
+++ src/components/movies/moviesPage.js
@@ -3,19 +3,21 @@ import {ToastContainer, toast} from "react-toastify";
 import {connect} from 'react-redux'
 import * as moviesAction from '../../actions/getMoviesAction'
 import MovieMidware from '../../middleware/moviesMiddleware'
+import { element } from "./C:/Users/moeed/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/prop-types";
 
 class MoviesPage extends Component {
 
     constructor(props, context) {
         super(props, context)
         this.state = {
-            movies: ''
+            movies: '',
+            MovieArray : []
         }
     }
 
-    movieRow(movie, index) {
+    movieRow(movie) {
         //This just like an adapter, dynamically adds rows with data
-        return <p key={index}>Title:{movie.title}
+        return <p>Title:{movie.title}
             and Release Year: {movie.releaseYear}</p>
     }
 
@@ -24,25 +26,43 @@ class MoviesPage extends Component {
         this.props.getData()
     }
     
-    // <div>{this.props.courses.map(this.courseRow)}</div>
+    // <div>{this.props.movieData}</div>
+
+    componentWillMount() {
+        //Fired on Page Load
+
+        setTimeout(()=>{
+            console.log(this.state.MovieArray  , "stateeeeeeeeeeeee")
+        }  , 600)
+
+
+    }
+
+    componentWillReceiveProps(prop){
+        console.log(prop.PageProps.movieData.movies)
+        this.setState({MovieArray : prop.PageProps.movieData.movies})
+       
+    }
+
     render() {
         return (
             <div className="container jumbotron">
                 <h1>Movies</h1>
                 <div className="row">
-                    <h2>Movies List</h2>
                     <button className="btn btn-primary" onClick={this.getMovie}>Get Movies</button>
                     <h2>Movies List:</h2>
-                    <div>{this.props.movieData}</div>
-
-                </div>
+                     <div>{
+                         this.state.MovieArray.forEach((element)=>{
+                             this.movieRow(element)
+                         })}</div>
+                </div>  
             </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    return {movies: state}
+    return { PageProps: state }
 }
 
 function mapDispatchToProps(dispatch){
